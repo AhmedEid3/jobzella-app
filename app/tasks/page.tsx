8d39a3ef993286fe93a8c363a5a3ca605300cb87
AhmedEid3 <ahmed.eid3@outlook.com>
2023-07-18 00:55:41 +0300

    Create an initial Take page.

---
+++ app/tasks/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@mui/material';
+
+const TasksPage = () => {
+  return (
+    <Box sx={{ backgroundColor: 'overlay.main', padding: 10 }}>TasksPage</Box>
+  );
+};
+
+export default TasksPage;
