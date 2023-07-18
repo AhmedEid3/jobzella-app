de4086844e1850fadf370980188bbae7dcb0e84d
AhmedEid3 <ahmed.eid3@outlook.com>
2023-07-18 14:52:17 +0300

    Update Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+import { Box, Link as MuiLink, Typography } from '@mui/material';
 
-export default function HomePage() {
+const HomePage = () => {
   return (
-    <Box>
-      <Typography>Home Page</Typography>
+    <Box
+      sx={{
+        backgroundColor: 'primary.main',
+        height: '100vh',
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        pb: '2rem',
+      }}
+    >
+      <Box m={'auto'}>
+        <Typography align="center" color={'white'} variant="h1">
+          Home Page
+        </Typography>
+
+        <MuiLink component={Link} href={'/login'}>
+          <Typography
+            align="center"
+            color={'text.primary'}
+            sx={{ textDecoration: 'underline' }}
+          >
+            Login Page
+          </Typography>
+        </MuiLink>
+
+        <MuiLink component={Link} href={'/tasks'}>
+          <Typography
+            align="center"
+            color={'text.primary'}
+            sx={{ textDecoration: 'underline' }}
+          >
+            Tasks Page
+          </Typography>
+        </MuiLink>
+      </Box>
     </Box>
   );
-}
+};
+
+export default HomePage;
